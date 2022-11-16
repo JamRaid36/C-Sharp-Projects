ebd7152b19708827ebcf28eb383ffdc9fc305bab
JamRaid36 <105257619+JamRaid36@users.noreply.github.com>
2022-11-15 23:16:35 -0800

    Added Constructor_Submission_Assignment

---
+++ Constructor Submission Assignment/Constructor Submission Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string castleName = "Castlevania"; // const variable
+            var faveFood = "Pizza"; // variable with var keyword
+
+
+            User newUser = new User("Master Chief"); // instantiates user class and utilizes chained constructor
+
+        }
+    }
+}
---
+++ Constructor Submission Assignment/Constructor Submission Assignment/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Submission_Assignment
+{
+    public class User
+    {
+       //Properties of user
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public User(string UserName) :this(UserName, "Password")//Constructor with default password string chained
+        {
+
+        }
+       
+        public User(string UserName, string Password)  //main constructor
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+
+       
+
+    }
+}
