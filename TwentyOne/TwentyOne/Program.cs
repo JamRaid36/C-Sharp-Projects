33a2562447353ade606f7c8636d434f25f6a1737
JamRaid36 <105257619+JamRaid36@users.noreply.github.com>
2022-11-15 19:18:11 -0800

    Added classes

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -10,6 +11,34 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-        }
+            //string text = "Here is some text.";
+            //File.WriteAllText(@"C:\Users\guita\Desktop\21 Game Log\log.txt", text); //writes text to a txt file if file doesnt exist it creates the txt file.
+
+            //string text = File.ReadAllText(@"C:\Users\guita\Desktop\21 Game Log\log.txt"); // Read txt file
+
+            
+
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name?");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello {0}. Would you like to join a game of 21 right now", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya") 
+            {
+                Player player = new Player(playerName,bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0) 
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.ReadLine();
+        }        
     }
 }
